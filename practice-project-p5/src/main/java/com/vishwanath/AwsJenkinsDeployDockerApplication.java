d5372d85ca9f3c4f17d734b7aacf27d05ae23264
Lenovo <Lenovo@DESKTOP-RPKE00A>
2022-03-13 16:17:51 +0530

    initial

---
+++ practice-project-p5/src/main/java/com/vishwanath/AwsJenkinsDeployDockerApplication.java
@@ -0,0 +1,13 @@
+package com.vishwanath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJenkinsDeployDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsJenkinsDeployDockerApplication.class, args);
+	}
+
+}
---
+++ practice-project-p5/src/test/java/com/vishwanath/AwsJenkinsDeployDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vishwanath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsJenkinsDeployDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
